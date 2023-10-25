d76ad19f18fb5c54c06dc3f2453bb44b9b604c34
FLKO1 <83712998+FLKO1@users.noreply.github.com>
2023-10-25 11:58:50 -0300

    aa

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -8,15 +8,17 @@ class CitaController extends Controller
 {
     public function index(){
         $citas = Cita::paginate();
-        return view("");
+        return view('citas.index', compact('citas'));
     }
 
     public function create(){
-        return view("");
+        return view('citas.create');
     }  
     
-    public function show(){
-        return view("");
+    public function show($id){
+        $cita= Cita::find($id);
+
+        return view('citas.show', compact('citas'));
     }
     
 
---
+++ resources/views/Cita/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cita')
+  
+@section('content')
+
+    <h1> Estas creando una cita</h1>
+    <form action="{{route('citas.store')}}" method="POST">
+        @csrf
+        <label>
+            <div class="form-group">
+                <label for="fecha">fecha:</label>
+                <input type="date" id="fecha" name="fecha" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="hora_disponible">Horas Disponibles:</label>
+                <input type="time" id="hora_disponible" name="hora_disponible"class="form-control" required>
+            </div>
+            <div class="form-group">  
+                <label for="especialidad_id">Especialidad</label>
+                <select id="especialiadad_id" name="especialidad_id" class="form-control" required>
+                    @foreach($especialidades as Especialidad)
+                    <option value="{{$especialidad->id}}">{{especialidad->nameespecialidades}}</option>
+                    @endforeach
+                </select>
+            </div>      
+    </form>
+@endsection
